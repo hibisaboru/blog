5342b0fa60a74eb109e7c84b55ae2d44c513556b
hibisaboru <y.chiba0527@gmail.com>
2026-02-22 00:05:10 +0900

    update: 2026-02-22 00:05

--- deploy.ps1
+++ deploy.ps1
@@ -1,24 +1,24 @@
-# deploy.ps1（hibisaboru.github.io に置く必要はなし。どこでもOK）
+# deploy.ps1（blogフォルダに置き、blogから実行する）
 
 # ===== Hugoビルド =====
-cd ../hibisaboru.com
+cd ..
+cd ./blog
 hugo
 
-# ===== blogリポジトリ（= hibisaboru.com）をpush =====
-
+# ===== blogリポジトリ（= ソース）をpush =====
 if (-not (git diff --quiet)) {
   git add -A
   git commit -m "update: $(Get-Date -Format 'yyyy-MM-dd HH:mm')"
-  git push origin main # 通常push。強制ではない
+  git push origin main
 }
 
 # ===== GitHub Pages用リポジトリにpublicをコピーしてpush =====
 cd ../hibisaboru.github.io
-Copy-Item ../hibisaboru.com/public/* -Destination . -Recurse -Force
+Copy-Item ../blog/public/* -Destination . -Recurse -Force
 
 git add -A
 git commit -m "deploy: $(Get-Date -Format 'yyyy-MM-dd HH:mm')"
 git push origin main
 
 # ===== 作業リポジトリに戻る =====
-cd ../hibisaboru.com
+cd ../blog
